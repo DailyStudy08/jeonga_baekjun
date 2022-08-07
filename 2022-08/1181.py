65a9670cd9420f509bb95961f59a2c99aa0c40ad
J.A <ja1won111@gmail.com>
2022-08-07 22:15:20 +0900

    1181

---
+++ 2022-08/1181.py
@@ -0,0 +1,16 @@
+import sys
+N = int(sys.stdin.readline())
+mylst = [sys.stdin.readline().strip() for i in range(N)]
+
+a = []
+for i in range(N):
+    a.append((len(mylst[i]), mylst[i]))
+a.sort()
+b=[]
+for i in a:
+    if i not in b: 
+        b.append(i)
+c=[]
+for i in range(len(b)):
+    c.append(b[i][1])
+print('\n'.join(c))  

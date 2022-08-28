295cb0ee3568fbb8bf58b7f293815007c515d5c3
J.A <ja1won111@gmail.com>
2022-08-28 21:20:39 +0900

    11721 solution-2

--- 2022-08/11721.py
+++ 2022-08/11721.py
@@ -1,6 +1,20 @@
+# 10 자리씩 끊어 출력하기
+# 1 - 슬라이싱 이용하기
 s = input()
 while True:
     if len(s) == 0:
         break
     print(s[:10])
     s = s[10:]
+
+# 2 - 인덱스에 모듈러스 연산을 사용한 반복문을 이용하기기
+s = input()
+
+for i in range(len(s)):
+    print(s[i],end='')
+    if (i%10 ==9):
+        print()
+
+'''
+두 경우에 있어서, 사용하는 메모리나 연산시간은 같다고 하네요
+'''
